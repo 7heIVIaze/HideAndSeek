9c84c1f8dfb4dd24e144af0eb3cf093b4bd201f0
7heIVIaze <kimdave20@naver.com>
2024-08-13 16:48:39 +0900

    Change the hierarchy
    소스코드 폴더 계층 구조를 조금 더 보기 좋게 변경함.

--- Source/HideAndSeek/Private/DoorInterface_cpp.cpp
+++
@@ -1,5 +0,0 @@
-// CopyrightNotice 2023 Sunggon Kim kimdave205@gmail.com. All Rights Reserved.
-
-#include "DoorInterface_cpp.h"
-
-// Add default functionality here for any IDoorInterface_cpp functions that are not pure virtual.
--- Source/HideAndSeek/Private/HideInterface.cpp
+++
@@ -1,5 +0,0 @@
-// CopyrightNotice 2023 Sunggon Kim kimdave205@gmail.com. All Rights Reserved.
-
-#include "HideInterface.h"
-
-// Add default functionality here for any IHideInterface functions that are not pure virtual.
